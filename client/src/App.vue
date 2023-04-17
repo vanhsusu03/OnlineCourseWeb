95cb4428f8d9d2f9f0c4db96dcb33d0aedc292cf
minhdatuet <minhdatuet@gmail.com>
2023-04-17 22:49:03 +0700

    test

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/img/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Home/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Home from './pages/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
