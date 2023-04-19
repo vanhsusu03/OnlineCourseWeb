ab54de9b62e42dd8e1c7b59a26152015710a43f0
Viet Anh Duong Nguyen <98511848+vanhsusu03@users.noreply.github.com>
2023-04-19 23:59:00 +0700

    update home router login signup

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,123 +1,496 @@
 <template>
-<!--  html code is here -->
-<div class="space"></div>
-<div class="top-cate">
-    <h2 class="top-title">Top categories</h2>
-    <div class="line-1">
-        <div class="cate">
-            <img src="../assets/img/address.png" class="cate-img">
-            <br/>
-            <p class="cate-content">Math</p>
+    <!--  html code is here -->
+    <!-- BACK GR -->
+    <div class="bgr">
+        <div class="top">
+            <img src="../assets/img/bgr1.jpg">
+            <div class="box">
+                <div class="text">
+                    <div class="heading">
+                        Learning your own courses
+                    </div>
+                    <div class="body">
+                        Unlock and try out our 1000+ courses
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="cate">
-            <img src="../assets/img/address.png" class="cate-img">
-            <br/>
-            <p class="cate-content">Literature</p>
+        <div class="bottom">
+            <img src="../assets/img/bgr2.jpg">
+            <div class="box">
+                <div class="text">
+                    <div class="heading">
+                        Empowering your skills
+                    </div>
+                    <div class="body">
+                        Best practice courses
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="cate">
-            <img src="../assets/img/address.png" class="cate-img">
-            <br/>
-            <p class="cate-content">Physical</p>
-        </div>
-        <div class="cate">
-            <img src="../assets/img/address.png" class="cate-img">
-            <br/>
-            <p class="cate-content">Chemistry</p>
-        </div>
-
     </div>
-    <div class="line-2">
-        <div class="cate">
-            <img src="../assets/img/address.png" class="cate-img">
-            <br/>
-            <p class="cate-content">Math</p>
+    <br>
+    <br>
+    <!-- TOP CATE -->
+    <div class="top-cate">
+        <h2 class="top-title">EXPLORE 1000+ COURSES IN DNA</h2>
+        <div class="underline"></div>
+        <br>
+        <div class="line-1">
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/math.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Math</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/des.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Design</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/dev.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Development</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/psy.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Psycology</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/music.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Music</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/health.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Health</p>
+            </div>
         </div>
-        <div class="cate">
-            <img src="../assets/img/address.png" class="cate-img">
-            <br/>
-            <p class="cate-content">Literature</p>
+        <div class="line-2">
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/lifestyle.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Lifestyle</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/media.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Media</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/it.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">IT/Software</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/economic.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Economic</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/chemistry.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Chemistry</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/physic.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Physics</p>
+            </div>
         </div>
-        <div class="cate">
-            <img src="../assets/img/address.png" class="cate-img">
-            <br/>
-            <p class="cate-content">Physical</p>
-        </div>
-        <div class="cate">
-            <img src="../assets/img/address.png" class="cate-img">
-            <br/>
-            <p class="cate-content">Chemistry</p>
-        </div>
-
     </div>
-</div>
+    <br>
+    <br>
+    <!-- REASONS FOR DNA -->
+    <div class="reasons">
+        <h2 class="top-title">WHY CHOOSING DNA?</h2>
+        <br>
+        <div class="line-1">
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/highquality.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">High quality</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/remote.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Remote studying</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/diver.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Courses diverse</p>
+            </div>
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/com.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">Large community</p>
+            </div>
+        </div>
+        <div class="line-1">
+            <div class="sub">
+                <div class="cate">
+                    <!-- <div class="math"> -->
+                    <img src="../assets/img/instructor.png" class="cate-img">
+                    <!-- </div> -->
+                    <br />
+                </div>
+                <p class="cate-content">'Open' Instructor</p>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
 </template>
 
-<script>    
-import Footer from '../components/Footer.vue'
+<script>
 export default {
     name: 'Home',
     // data() {
-	// 	return {
+    // 	return {
 
-	// 	}
-	// },
-	// methods: {
-	// 	...mapMutations(['scrollToTop'])
-	// },
-	// computed: {
-	// 	...mapState(['user'])
-	// }
+    // 	}
+    // },
+    // methods: {
+    // 	...mapMutations(['scrollToTop'])
+    // },
+    // computed: {
+    // 	...mapState(['user'])
+    // }
 }
+
 
 // Logic do in here
 </script>
 
 <style lang = "scss" scoped>
 @font-face {
-   font-family: myFirstFont;
-   src: url(../assets/font/Vollkorn-VariableFont_wght.ttf);
+    // `   @import url(https://www.myfonts.com/products/headline-bold-sole-serif-73078);
+    font-family: fontt;
+    src: url(../assets/font/Novecento\ WideDemiBold.ttf);
+    //    font-family: myFirstFont;
+    //    src: url(../assets/font/Vollkorn-VariableFont_wght.ttf);
 }
-*{
+
+* {
     position: relative;
     padding: 0;
     margin: 0;
     border-sizing: border-box;
-    font-family: myFirstFont;
+    font-family: fontt;
 }
 
-.line-1, .line-2 {
+.top-title {
+    margin-left: 30px;
+}
+
+// BACKGROUND ANIMATION
+.bgr {
+    position: relative;
+    top: 0;
+    left: 0;
+    bottom: 0;
+
+    .top {
+        position: relative;
+        top: 0;
+        left: 0;
+
+        img {
+            clip-path: inset(40px);
+            width: 95%;
+            height: 70%;
+            margin-left: 30px;
+        }
+
+        .box {
+            text-align: center;
+            position: absolute;
+            top: 150px;
+            left: 170px;
+            width: 35%;
+            height: 20%;
+            background-color: white;
+            box-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.5);
+
+            .text {
+                position: relative;
+
+                .heading {
+                    position: absolute;
+                    font-size: 225%;
+                    font-style: bold;
+                    font-weight: 1000;
+                    width: 100%;
+                    height: 25%;
+                }
+
+                .body {
+                    position: absolute;
+                    font-size: 150%;
+                    font-style: bold;
+                    font-weight: 500;
+                    margin-top: 20%;
+                }
+            }
+        }
+    }
+
+    .bottom {
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        img {
+            clip-path: inset(40px);
+            width: 95%;
+            height: 70%;
+            margin-left: 30px;
+            // margin-right: 40px;
+        }
+
+        .box {
+            text-align: center;
+            position: absolute;
+            top: 580px;
+            left: 750px;
+            width: 35%;
+            height: 20%;
+            background-color: white;
+            box-shadow: 0 1rem 1rem rgba(0, 0, 0, 0.5);
+
+            .text {
+                position: relative;
+
+                .heading {
+                    position: absolute;
+                    font-size: 225%;
+                    font-style: bold;
+                    font-weight: 1000;
+                    width: 100%;
+                    height: 25%;
+                    margin-top: 8%;
+                }
+
+                .body {
+                    position: absolute;
+                    font-size: 150%;
+                    font-style: bold;
+                    font-weight: 500;
+                    margin-top: 25%;
+                    margin-left: 100px;
+                }
+            }
+        }
+    }
+
+    @keyframes FadeOut {
+        0% {
+            opacity: 1;
+        }
+
+        45% {
+            opacity: 1;
+        }
+
+        55% {
+            opacity: 0;
+        }
+
+        100% {
+            opacity: 0;
+        }
+    }
+
+    .top {
+        animation-name: FadeOut;
+        animation-timing-function: ease-in-out;
+        animation-iteration-count: infinite;
+        animation-duration: 5s;
+        animation-direction: alternate;
+    }
+
+    @keyframes FadeIn {
+        0% {
+            opacity: 0;
+        }
+
+        45% {
+            opacity: 0;
+        }
+
+        55% {
+            opacity: 1;
+        }
+
+        100% {
+            opacity: 1;
+        }
+    }
+
+    .bottom {
+        animation-name: FadeIn;
+        animation-timing-function: ease-in-out;
+        animation-iteration-count: infinite;
+        animation-duration: 5s;
+        animation-direction: alternate;
+    }
+}
+
+.line-1,
+.line-2 {
+    // width: 100%;
+    // height: 100%;
+    position: relative;
     display: flex;
     margin-top: 15px;
+    justify-content: space-evenly;
 }
-.top-title {
-    font-size: 1.8rem;
-}
+
+
 .top-cate {
-    margin-left: 15px
-}
-.cate {
-    width: 100%;
-    display: inline-block;
-}
-.cate-img {
-    text-align: center;
-    width: 60%;
-    padding: 10%;
-    background-color: rgb(0,128,128);
-    cursor: pointer;
-    border-radius: 10px;
+    margin-left: 15px;
+
+    .top-title {
+        font-size: 1.8rem;
+        font-weight: 800;
+        color: rgb(52, 73, 94);
+    }
+
+    .sub {
+        text-align: center;
+
+        .cate {
+            width: 230px;
+            height: 230px;
+            display: inline-block;
+            background-color: rgb(250, 250, 250);
+            border: 1px solid black;
+            cursor: pointer;
+            border-radius: 10px;
+
+            .cate-img {
+                width: 75%;
+                top: 10%;
+                padding: 10%;
+            }
+
+            .cate-img:hover {
+                transform: scale(1.05);
+            }
+        }
+
+        .cate-content {
+            margin: 10px 0;
+            font-size: 1.2rem;
+            font-weight: 600;
+        }
+    }
 }
 
-.cate-img:hover {
-    padding: 3%;
-    width: 74%;
+.reasons {
+    .top-title {
+        font-size: 1.8rem;
+        font-weight: 800;
+        color: rgb(52, 73, 94);
+    }
+
+    .sub {
+        text-align: center;
+
+        .cate {
+            width: 230px;
+            height: 230px;
+            display: inline-block;
+            background-color: rgb(255,250,240);
+            border: 1px solid rgb(207,207,207);
+            cursor: pointer;
+            border-radius: 50%;
+            .cate-img {
+                width: 75%;
+                top: 10%;
+                padding: 10%;
+            }
+
+            .cate-img:hover {
+                transform: scale(1.05);
+            }
+        }
+
+        .cate-content {
+            margin: 10px 0;
+            font-size: 1.2rem;
+            font-weight: 600;
+        }
+    }
 }
 
-.cate-content {
-    margin: 10px 0;
-    font-size: 1.2rem;
-    font-weight: 600;
-}
 .space {
     min-height: 100px;
 }
