b33a5dc643d9deaf88f7969d1e08d51c6f8d97cb
Viet Anh Duong Nguyen <98511848+vanhsusu03@users.noreply.github.com>
2023-04-22 19:52:11 +0700

    update full login signup home

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,37 +1,37 @@
-// import router from "@/routes/router";
-// import axios from "axios"
+import router from "@/routes/router";
+import axios from "axios"
 
-// export default {
-//   async getProducts(context) {
-//     await axios.get('products', {withCredentials: true})
-//     .then((res) => {
-//       context.commit("setFoods", res.data);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     })
-//   },
+export default {
+  async getProducts(context) {
+    await axios.get('products', {withCredentials: true})
+    .then((res) => {
+      context.commit("setFoods", res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  },
 
-//   async checkLogin(context) {
-//     let res = await axios.get("login", { withCredentials: true });
-//     if (res.data.cookie) {
-//       if (router.currentRoute.value.path == "/login") {
-//         router.push("/");
-//       }
+  async checkLogin(context) {
+    let res = await axios.get("login", { withCredentials: true });
+    if (res.data.cookie) {
+      if (router.currentRoute.value.path == "/login") {
+        router.push("/");
+      }
 
-//       let data = await axios.get("info", { withCredentials: true });
+      let data = await axios.get("info", { withCredentials: true });
 
-//       context.commit("setUser", data.data);
-//       context.commit("setLogged", true);
+      context.commit("setUser", data.data);
+      context.commit("setLogged", true);
 
-//       if (data.data.role) {
-//         context.commit("setAdmin", "admin");
-//       }
-//       else {
-//         if (router.currentRoute.value.path == "/admin") {
-//           router.push('/');
-//         }
-//       }
-//     }
-//   }
-// }
+      if (data.data.role) {
+        context.commit("setAdmin", "admin");
+      }
+      else {
+        if (router.currentRoute.value.path == "/admin") {
+          router.push('/');
+        }
+      }
+    }
+  }
+}
