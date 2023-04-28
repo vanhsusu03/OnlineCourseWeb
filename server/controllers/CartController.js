286b37c9686a3c12f28ded8e9b801a39e634626f
hoangnguyen2003 <ninjadoc0152@gmail.com>
2023-04-28 16:02:35 +0700

    Add, remove Course in Cart

--- server/controllers/CartController.js
+++ server/controllers/CartController.js
@@ -2,11 +2,24 @@ const sequelize = require('sequelize');
 const { models: { Cart, Course, Instructor, Student } } = require('../models');
 
 class CartController {
+    constructor() {
+        this.addCourse = this.addCourse.bind(this);
+    }
+
+    isInCart(cart, id) {
+        for (let i = 0; i < cart.length; i++) {
+            if (id == cart[i].courseId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     async getInfo(req, res, next) {
-        req.session.studentId = 2;
         if (!req.session.cart) {
             const cart = await Cart.findAll({
                 attributes: [
+                    [sequelize.col('Course.course_id'), 'courseId'],
                     [sequelize.col('Course.title'), 'courseTitle'],
                     [sequelize.col('Course.description'), 'courseDescription'],
                     [sequelize.col('Course.image'), 'courseImage'],
@@ -51,6 +64,77 @@ class CartController {
             coursesInCart: cart,
             totalPrice: totalPrice
         });
+    }
+
+    async addCourse(req, res, next) {
+        const { 
+            courseId, courseTitle, courseDescription, courseImage, courseFee,
+            instructorFirstName, instructorLastName
+        } = req.body;
+
+        var course = {
+            courseId, courseTitle, courseDescription, courseImage, courseFee,
+            instructorFirstName, instructorLastName
+        };
+
+        req.session.totalPrice += courseFee;
+
+        var cart;
+        if (req.session.cart) {
+            cart = req.session.cart;
+            if (this.isInCart(cart, courseId)) {
+                return res.status(400).json({
+                    msg: 'The course already exists in the cart!',
+                    numberOfCourses: cart.length
+                });
+            } else {
+                cart.push(course);
+            }
+        } else {
+            cart = [course];
+        }
+
+        req.session.cart = cart;
+
+        return res.status(200).json({
+            msg: 'Add success!',
+            numberOfCourses: cart.length
+        });
+    }
+
+    async removeCourse(req, res, next) {
+        const courseId = req.body.courseId;
+        
+        var cart = req.session.cart;
+        var isRemoved = false;
+
+        for (let i = 0; i < cart.length; i++) {
+            if (courseId == cart[i].courseId) {
+                const deleteProduct = await Cart.destroy({
+                    where: { 
+                        student_id: req.session.studentId,
+                        course_id: courseId 
+                    }
+                });
+                req.session.totalPrice -= cart[i].courseFee;
+                cart.splice(i, 1);
+                isRemoved = true;
+                break;
+            }
+        }
+
+        if (!isRemoved) {
+            return res.status(400).json({
+                msg: 'Not found!',
+                numberOfCourses: cart.length
+            });
+        } else {
+            req.session.cart = cart;
+            return res.status(200).json({
+                msg: 'Remove success!',
+                numberOfCourses: cart.length
+            });
+        }
     }
 }
 
