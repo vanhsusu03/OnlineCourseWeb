bd1ef69ce14a81ab6f235f855db079e9f847abc6
hoangnguyen2003 <ninjadoc0152@gmail.com>
2023-04-28 16:14:06 +0700

    route for adding course and removing course

--- server/routes/cart_route.js
+++ server/routes/cart_route.js
@@ -4,6 +4,7 @@ const { isAuth } = require('../middleware/Authentication');
 const router = express.Router();
 
 router.get('/students/:studentId/cart', isAuth, cartController.getInfo);
-
+router.post('/students/:studentId/cart/:courseId', isAuth, cartController.addCourse);
+router.delete('/students/:studentId/cart/:courseId', isAuth, cartController.removeCourse);
 
 module.exports = router;
