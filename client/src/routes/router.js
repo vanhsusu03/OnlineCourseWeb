e897fc16cb0f4f0b8de6ad851384ec970dbca2e9
minhdatuet <minhdatuet@gmail.com>
2023-05-01 20:32:01 +0700

    update deposit

--- client/src/routes/router.js
+++ client/src/routes/router.js
@@ -4,6 +4,7 @@ import { createWebHistory, createRouter } from "vue-router";
 import Home from '../pages/Home.vue';
 import Login from '../pages/Login.vue';
 import Register from '../pages/Register.vue';
+import Deposit from '../pages/Deposit.vue';
 // import Cart from '../pages/Cart.vue';
 // import StudentInfo from '../pages/StudentInfo.vue';
 // import EditInfo from '../components/EditInfo.vue';
@@ -15,6 +16,7 @@ const routes = [
     {  path: "/", name: "Home", component: Home},
     {  path: "/login", name: "Login", component: Login},
     {  path: "/signup", name: "SignUp", component: Register},
+    {  path: "/deposit", name: "Deposit", component: Deposit},
     // {  path: "/student/info", name: "StudentInfo", component: StudentInfo},
     // {  path: "/cart", name: "Cart", component: Cart},
     // {  path: "/editinfo", name: "EditInfo", component: EditInfo},
