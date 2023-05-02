6ec9e1109e6da93d6711b0986137bda901e572e4
duyhehehe <duy20036athcsns@gmail.com>
2023-05-02 16:45:55 +0700

    Update searching: search by category

--- server/routes/student_route.js
+++ server/routes/student_route.js
@@ -3,8 +3,8 @@ const studentController = require('../controllers/StudentController');
 const { isAlreadyLogin, isAuth, isInstructor} = require('../middleware/Authentication');
 const router = express.Router();
 
-router.post('/signup', isAlreadyLogin, studentController.signUp);
-router.post('/login',isInstructor, studentController.logIn);
+router.post('/signup', studentController.signUp);
+router.post('/login', studentController.logIn);
 router.post("/logout",studentController.logOut);
 
 
