1b8542aa87dc4ca3e46218c2c555c0872b6b60d2
minhdatuet <minhdatuet@gmail.com>
2023-05-14 17:28:09 +0700

    remove unused files

--- server/routes/admin_route.js
+++ server/routes/admin_route.js
@@ -3,18 +3,9 @@ const adminController = require('../controllers/AdminController');
 const { isAdmin } = require('../middleware/Authentication');
 const router = express.Router();
 
-// router.post('/admin/:studentId', isAdmin, adminController.deleteAccount);
-// router.post('/admin/:courseId', isAdmin, adminController.deleteCourse);
-// router.post('/admin/:studentId', isAdmin, adminController.editStudentInfo);
-// router.post('/admin/:courseId', isAdmin, adminController.editCourse);
-// router.get('/admin/courses', isAdmin, adminController.showCourses);
-// router.get('/admin/accounts', isAdmin, adminController.showAccounts);
-
-
 router.delete('/admin/delete/:studentId', isAdmin, adminController.deleteAccount);
 router.delete('/admin/delete/course/:courseId', isAdmin, adminController.deleteCourse);
 router.post('/admin/change/:studentId/:coin', isAdmin, adminController.editStudentInfo);
-router.post('/admin/change/:courseId', isAdmin, adminController.editCourse);
 router.get('/admin/courses', isAdmin, adminController.showCourses);
 router.get('/admin/accounts', isAdmin, adminController.showAccounts);
 router.get('/admin/instructors', isAdmin, adminController.showAllInstructors)
