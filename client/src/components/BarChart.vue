ff86d25973bb26e0bdd180812c56bfcfed716e4b
ctpanh <ctpanh03@gmail.com>
2024-05-17 00:07:33 +0700

    feat(fe): add revenue report, course report

---
+++ client/src/components/BarChart.vue
@@ -0,0 +1,66 @@
+<template>
+  <Bar v-if="loaded" :options="chartOptions" :data="chartData" />
+</template>
+
+<script>
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+export default {
+  name: "BarChart",
+  components: { Bar },
+  props: {
+    dataChart: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      chartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ],
+        datasets: [
+          {
+            label: "$",
+            data: this.dataChart,
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+      },
+      loaded: true,
+    };
+  },
+};
+</script>
